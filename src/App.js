a602c2dbdde863f87674bab71e48b83871c26fe6
amadi5892 <amadi5892@gmail.com>
2022-10-26 17:11:39 -0400

    introduced the .map function

--- src/App.js
+++ src/App.js
@@ -8,33 +8,32 @@ constructor() {
   super();
 
   this.state = {
-    name: {firstName: 'Parrish', lastName: 'Williamson'},
-    company: 'ZTM'
+    monsters: [
+      {
+        name: 'Linda'
+      },
+      {
+        name: 'Frank'
+      },
+      {
+        name: 'Jacky'
+      },
+      {
+        name: 'Siggy'
+      }
+    ]
+    
   }
 }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} { this.state.name.lastName }, I work at {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState(() => {
-              return {
-                name: { firstName: 'Siggy', lastName: 'Lloyd' },
-              }
-            }, 
-            () => {
-              console.log(this.state)
-            })
-            
-          }}>
-            Change Name
-          </button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>;
+          })
+        }
       </div>
     );
   }
