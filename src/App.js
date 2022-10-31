ce47ddfdf27e2b3808d98a99199111998dff1852
amadi5892 <amadi5892@gmail.com>
2022-10-31 12:21:30 -0400

    converted class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
@@ -6,13 +6,42 @@ import './App.css';
 
 // functional components use the arrow function; there are no lifecycles when it comes to functional components;
 const App = () => {
+  console.log('render')
+  const [searchField, setSearchField] = useState(''); //[value, setValue]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+
+  }, [monsters, searchField]);
+
+  
+
+  const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLocaleLowerCase();
+  setSearchField(searchFieldString);
+
+  
+        
+  }
   return (
     <div className="App">
         <h1 className="app-title">Monsters Rolodex</h1>
 
-        {/* <SearchBox onChangeHandler={onSearchChange} placeholder='search monster' className='search-box' />
-        <CardList monsters={filteredMonsters} /> */}
+        <SearchBox onChangeHandler={onSearchChange} placeholder='search monster' className='search-box' />
+        <CardList monsters={filteredMonsters} />
       </div>
   )
 }
